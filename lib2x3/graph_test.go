59c4a12a31a40b62065de471937b56f1d1abdcd8
Drew O'Meara <drew@plan-systems.org>
2023-09-29 21:55:18 -0500

    moar test graphs

--- lib2x3/graph_test.go
+++ lib2x3/graph_test.go
@@ -9,36 +9,43 @@ import (
 	"github.com/2x3systems/go2x3/lib2x3"
 )
 
-
 func TestBasics(t *testing.T) {
 
+	PrintCycles("1")
+	PrintCycles("1^")
+	PrintCycles("1^^")
+	PrintCycles("1^^^")
+
+	PrintCycles("1~2-3~5-~6-4~2") // E0 <=> C1
+
 	PrintCycles("1^^-2-3~4^^,3-5")
-	
+
 	PrintCycles("1-2-3=4-5=6-7=8-1")
 	PrintCycles("1^-2-3-4-2,1-4") // K4 v2
 
 	PrintCycles("1-2=3-4=5")
-	
+
 	PrintCycles("1---2")
 	PrintCycles("1--~2")
 	PrintCycles("1-~~2")
 	PrintCycles("1~~~2")
 
+	PrintCycles("1-2-3")
+	PrintCycles("1~2-3")
+	PrintCycles("1~2~3")
+
 	PrintCycles("1~2~3-1-4-5-2,3-6~7~4,5-8~6,7-8")
-	
-	PrintCycles("1^-2~3~6-7^-8~5~4-2,6-8,1-4")                         //
+
+	PrintCycles("1^-2~3~6-7^-8~5~4-2,6-8,1-4") //
 
 	PrintCycles("1^-~2~3~~4")
-	
-
-	PrintCycles("1~2-3")
 
 	PrintCycles("1^-~2-3=4")
 
 	PrintCycles("1-2,2=3")
 	PrintCycles("1-2,1-4,2-3,2-4,3-4")
 
-	PrintCycles("1~2-3~1-4-5-2,3-6-7~4,5-8-6,7-8")                         //
+	PrintCycles("1~2-3~1-4-5-2,3-6-7~4,5-8-6,7-8") //
 	// PrintCycles("1-2, 1-3, 1-4, 2-5, 4-5, 2-6, 3-6, 3-7, 4-7, 5-8, 6-8, 7-8") // 001
 	// PrintCycles("1~2, 1~3, 1~4, 2-5, 3-5, 2-6, 4-6, 3-7, 4-7, 5~8, 6~8, 7~8") // 002
 	// PrintCycles("1~2, 1~3, 1~4, 2-5, 4-5, 2-6, 3-6, 3-7, 4-7, 5-8, 6-8, 7-8") // 003
